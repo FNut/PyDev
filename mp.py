c2c3d103df379569b9151d3c63970bfac9fa2448
FNut <63280589+FNut@users.noreply.github.com>
2021-02-07 08:38:13 +0300

    Add files via upload

---
+++ mp.py
@@ -0,0 +1,31 @@
+print('Math Puzzles')
+print('')
+
+print('LVL 1')
+print('2 + 2')
+ans1 = input('Ответ: ')
+if ans1 == ('4'):
+	print('Done')
+	print('')
+	
+	print('LVL 2')
+	print('a = 1')
+	print('b = 2a')
+	ans2 = input('b = ')
+	if ans2 == ('2'):
+		print('Done')
+		print('')
+		
+		print('LVL 3')
+		print('x + x')
+		ans3 = input('Ответ: ')
+		if ans3 == ('2x'):
+			print('Done')
+			print('')
+			
+			print('LVL 3')
+			print('0 / 1')
+			ans3 = input('Ответ: ')
+			if ans3 == ('0'):
+				print('Done')
+				print('')
