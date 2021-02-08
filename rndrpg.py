457fe108ca0c7c81364c63e801bcadf41a189c0a
FNut <geimers93@mail.ru>
2021-02-08 17:35:30 +0300

    Create rndrpg.py

---
+++ rndrpg.py
@@ -0,0 +1,15 @@
+import random
+from random import randint
+print('Random PG')
+print('')
+name = []
+print('Name: ', random.choice(name))
+print('Money: ', randint(0, 100))
+print('HP: ', randint(0, 100))
+print('IQ: ', randint(0, 100))
+magic = ['Fire', 'Water']
+print('Magic: ', random.choice(magic))
+print('LVL: ', randint(0, 100))
+pet = ['Dog', 'Cat', 'Dragon']
+print('Pet: ', random.choice(pet))
+ext = input('')
