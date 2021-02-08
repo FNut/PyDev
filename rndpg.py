e18b99844f68e1fc82b745e99a95e569e361f289
FNut <geimers93@mail.ru>
2021-02-08 21:06:18 +0300

    Update rndpg.py

--- rndpg.py
+++ rndpg.py
@@ -4,12 +4,12 @@ print('Random PG')
 print('')
 name = ['Alex', 'Max']
 print('Name: ', random.choice(name))
-print('Money: ', randint(0, 100))
 print('HP: ', randint(0, 100))
+print('LVL: ', randint(0, 100))
+print('Money: ', randint(0, 100))
 print('IQ: ', randint(0, 100))
 magic = ['Fire', 'Water']
 print('Magic: ', random.choice(magic))
-print('LVL: ', randint(0, 100))
 pet = ['Dog', 'Cat', 'Dragon']
 print('Pet: ', random.choice(pet))
 ext = input('')
